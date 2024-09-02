61929f5fbc9e7c74352dd39c0f4498bcb9de4599
Robby <gavigan.4@osu.edu>
2024-09-02 15:12:17 -0400

    Reddit API Initialization

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import os
 import praw
+from dotenv import load_dotenv
+
+# loading env variables
+load_dotenv()
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+user_agent = os.getenv('USER_AGENT')
+
+# setting up reddit instance
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent
+)
+
+# test
+# subreddit = reddit.subreddit('python')
+# for post in subreddit.hot(limit=5):
+#     print(post.title)
+
